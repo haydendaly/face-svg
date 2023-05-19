41fc0314fc48326a1ba5618bd28d64bb7b93a73f
haydendaly <hcd36@cornell.edu>
2023-05-18 20:54:13 -0400

    fix

--- hardware/main.py
+++ hardware/main.py
@@ -12,19 +12,7 @@ def givePoint(target):
     url = 'https://hw3-bice.vercel.app/api/points/4242/0'
     data = {'target': target}
     response = requests.post(url, json=data)
-
-    if response.status_code == 200:
-        if target == 0:
-            global ball_count_0
-            ball_count_0 += 1
-            print(f"Ball {ball_count_0} detected for target 0!")
-        elif target == 1:
-            global ball_count_1
-            ball_count_1 += 1
-            print(f"Ball {ball_count_1} detected for target 1!")
-    else:
-        print(f"Error sending POST request: {response.status_code}")
-
+    print(response)
 
 max_so_far = 0
 while True:
